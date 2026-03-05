388b689d87eb58067f62236fcf332b613c363f98
Rashmika Sharma <rashmikasharma913@gmail.com>
2026-03-05 22:09:18 +0530

    Added website frontend for ticket booking system

--- src/app.js
+++ src/app.js
@@ -1,14 +1,21 @@
 import express from "express";
-import bookingRoutes from "./modules/booking/booking.route.js"
+import path from "path";
+import { fileURLToPath } from "url";
+import bookingRoutes from "./modules/booking/booking.route.js";
 
 const app = express();
 
+// Fix for __dirname in ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// middleware
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("🚀 Concurrent Ticket Booking API Running");
-});
+// serve frontend
+app.use(express.static(path.join(__dirname, "../public")));
 
-app.use('/api', bookingRoutes);
+// API routes
+app.use("/api", bookingRoutes);
 
 export default app;
