b0d465f62c337e3bb45d6d92e9dc3087b0fa9671
Rashmika Sharma <rashmikasharma913@gmail.com>
2026-03-05 15:17:25 +0530

    Modified index.js

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import app from "./src/app.js";
 import { connectRedis } from "./src/config/redis.js";
 
-await connectRedis();
-
-app.listen(3000, () => {
-    console.log("Server started successfully");
-})
+const startServer = async () => {
+    await connectRedis();
+    app.listen(3000, () => {
+        console.log("Server started successfully");
+    })
+}
+startServer();
